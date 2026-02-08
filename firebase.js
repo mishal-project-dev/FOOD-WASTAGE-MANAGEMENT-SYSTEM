cf9e6454beae0917aea0f5733396010b27e647db
MISHAL AGRAWAL <agrawalmishal9598@gmail.com>
2026-02-08 12:27:11 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDE10peMacNHExnSr4cTY6FhciJsWfTSA0",
+  authDomain: "food-waste-platform-55599.firebaseapp.com",
+  projectId: "food-waste-platform-55599",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
